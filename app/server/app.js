bc6469b6da1a0efe555293291b9794e85f86ff2d
Te Vallee <astronomemusic@gmail.com>
2014-12-20 15:21:43 -1000

    cleaning for merge

--- app/server/app.js
+++ app/server/app.js
@@ -118,3 +118,9 @@ app.delete('/', function (req, res){
 
   
 });
+
+var server = app.listen(3000, function (){
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port)
+});
