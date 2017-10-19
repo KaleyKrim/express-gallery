761cb54719f58ba9251a21c7037911a76d847685
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-10-18 16:06:23 -1000

    makes link column unique

--- models/photo.js
+++ models/photo.js
@@ -3,7 +3,7 @@
 module.exports = function(sequelize, DataTypes) {
   const Photo = sequelize.define('photo', {
     author: DataTypes.STRING,
-    link: DataTypes.STRING,
+    link: {type: DataTypes.STRING, unique: true},
     description: DataTypes.TEXT
   }, {
     tableName: 'photos'
