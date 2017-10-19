a438dd51b1ce5c63d9660d2016985856bc33a566
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-18 14:31:22 -1000

    server's up

---
+++ server.js
@@ -0,0 +1,15 @@
+//jshint esversion:6
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const port = process.env.port || 8080;
+const db = require('./models');
+const User = db.user;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  db.sequelize.sync({ force: false });
+  console.log(`Server listening on port: '${port}`);
+});
