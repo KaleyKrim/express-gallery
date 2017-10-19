ab70d3e92d2e61383169997e671d1e1014c5c9a1
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-18 15:00:24 -1000

    post test

--- server.js
+++ server.js
@@ -3,15 +3,30 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 const port = process.env.port || 8080;
 const db = require('./models');
-const User = db.user;
+const Photo = db.photo;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.get('/', (req, res) => {
+app.engine('.hbs', exphbs({
+  defaultLayout: 'main',
+  extname:'.hbs'
+}));
 
-// });
+app.set('view engine', '.hbs');
+
+app.post('/gallery', (req, res) => {
+  const author = req.body.author;
+  const link = req.body.link;
+  const description = req.body.description;
+
+  return Photo.create({ author: author, link: link, description: description})
+    .then(newPhoto => {
+      return res.json(newPhoto);
+    });
+});
 
 app.listen(port, () => {
   db.sequelize.sync({ force: false });
