21d263eda0d6aa6bb3c9c17424fc6b7982f6b140
Kristin Takehara <KRISTINTAKEHARA@Kristins-MacBook-Pro.local>
2017-10-19 15:43:37 -1000

    set up secret/register/login page

--- models/photo.js
+++ models/photo.js
@@ -1,13 +1,19 @@
 //jshint esversion: 6
 
 module.exports = function(sequelize, DataTypes) {
-  const Photo = sequelize.define('photo', {
+  const photo = sequelize.define('photo', {
     author: DataTypes.STRING,
     link: {type: DataTypes.STRING, unique: true},
     description: DataTypes.TEXT
-  }, {
-    tableName: 'photos'
   });
+  photo.associate = function(models){
+    photo.belongsTo(models.user, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: true
+      }
+    });
+  };
 
-  return Photo;
+  return photo;
 };
--- models/user.js
+++ models/user.js
@@ -1,12 +1,14 @@
 //jshint esversion: 6
 
 module.exports = function(sequelize, DataTypes){
-  const User = sequelize.define('user', {
+  const user = sequelize.define('user', {
     username: { type : DataTypes.STRING, unique: true},
     password: DataTypes.STRING
-  }, {
-    tableName: 'users'
   });
 
-  return User;
+  user.associate = function(models) {
+    user.hasMany(models.photo);
+  };
+
+  return user;
 };
