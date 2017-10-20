1960c56689fe0a25aea9c74084b2f1091b099ee2
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-10-19 14:47:58 -1000

    unused users routes, just in case

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+// jshint esversion: 6
+
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const User = db.user;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
+
+
+
+
+module.exports = router;
