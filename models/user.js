1832238b3702ab6e510a0dc4e204024dd2666fa4
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-10-21 11:43:56 -1000

    adds roles column to user

--- models/user.js
+++ models/user.js
@@ -3,7 +3,8 @@
 module.exports = function(sequelize, DataTypes){
   const user = sequelize.define('user', {
     username: { type : DataTypes.STRING, unique: true},
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    roles: DataTypes.STRING
   });
 
   user.associate = function(models) {
