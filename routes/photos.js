9d4960d3b9f44ddcb354656893a94e3bba3029bc
Kaley Kriminger <kaley.kriminger@gmail.com>
2017-10-21 15:25:37 -1000

    adds new error handling, fixes bug in photo detail page

--- routes/photos.js
+++ routes/photos.js
@@ -14,22 +14,21 @@ router.get('/', (req, res) => {
     return res.render('./index', locals);
   })
   .catch((err) => {
-    let message = { message : 'Unable to locate your photo. Please try again.'};
+    let message = { message : 'Unable to locate photo! Please try again.'};
     res.render('./error', message);
   });
 });
 
 //create new gallery photo and post to gallery
 router.post('/', (req, res) => {
-  console.log('req.user', req.user);
+
   const author = req.body.author;
   const link = req.body.link;
   const description = req.body.description;
-//link must be an http url
+
   if(link.includes('http')) {
     return Photo.create({ author: author, link: link, description: description, userId: req.user.id})
     .then(newPhoto => {
-      // //res.render()
       return res.redirect('/gallery');
     })
     .catch((err) => {
@@ -42,23 +41,23 @@ router.post('/', (req, res) => {
   }
 });
 
-//get new photo
+
 router.get('/new', (req, res) => {
   res.render('./new');
 });
 
-//get photo by id in gallery
+
 router.get('/:id', (req, res) => {
   const photoId = req.params.id;
 
   return Photo.findAll({
     where: {
-      id: [(photoId), (parseInt(photoId)+1), (parseInt(photoId)+2), (parseInt(photoId+3))]
+      id: [(photoId), (parseInt(photoId)+1), (parseInt(photoId)+2), (parseInt(photoId)+3)]
     },
   })
   .then(photo => {
-    console.log('photo', photo);
     let locals = { mainPhoto : photo[0], photos : [photo[1], photo[2], photo[3]]};
+
     return res.render('./photo', locals);
   });
 });
@@ -66,6 +65,7 @@ router.get('/:id', (req, res) => {
 router.put('/:id', (req, res) => {
   let newInfo = req.body;
   let photoId = req.params.id;
+
   return Photo.findById(photoId)
   .then(photo => {
     if(req.user.id === photo.userId){
@@ -81,6 +81,7 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   let photoId = req.params.id;
+
   return Photo.findById(photoId)
   .then(photo => {
     if(req.user.id === photo.userId){
@@ -94,14 +95,18 @@ router.delete('/:id', (req, res) => {
   });
 });
 
-//render edit page for photos
+
 router.get('/:id/edit', (req, res) => {
   const photoId = req.params.id;
-  console.log(req.params.id);
   return Photo.findById(photoId)
     .then(photo => {
-      let locals = photo.dataValues;
-      return res.render('./edit', locals);
+      if(req.user.id === photo.userId){
+        let locals = photo.dataValues;
+        return res.render('./edit', locals);
+      }else{
+        let message = { message : `Sorry, you aren't logged in to the account that created this photo. Please log in and try again.`};
+        res.render('./error', message);
+      }
     });
 });
 
